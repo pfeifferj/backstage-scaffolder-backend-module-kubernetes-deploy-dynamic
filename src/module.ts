834305a00bf61ebe46e224550a9fee888b18dd72
Josephine Pfeiffer <hi@josie.lol>
2024-02-15 14:54:05 +0100

    bump

---
+++ src/module.ts
@@ -0,0 +1,31 @@
+import {
+	createBackendModule,
+	coreServices,
+} from '@backstage/backend-plugin-api';
+import { ScmIntegrations } from '@backstage/integration';
+import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
+import { deployKubernetesAction } from '@pfeifferj/plugin-scaffolder-backend-module-kubernetes-deploy';
+import { readFileAction } from '@pfeifferj/plugin-scaffolder-backend-module-read-file';
+/**
+ * @public
+ * The Read File Module for the Scaffolder Backend
+ */
+export const readFileModule = createBackendModule({
+	moduleId: 'read-file',
+	pluginId: 'scaffolder',
+	register({ registerInit }) {
+		registerInit({
+			deps: {
+				scaffolderActions: scaffolderActionsExtensionPoint,
+				config: coreServices.rootConfig,
+			},
+			async init({ scaffolderActions, config }) {
+				const integrations = ScmIntegrations.fromConfig(config);
+				scaffolderActions.addActions(
+					deployKubernetesAction(),
+					readFileAction()
+				);
+			},
+		});
+	},
+});
