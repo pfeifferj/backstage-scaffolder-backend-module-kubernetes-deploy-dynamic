140dc60f024686abeb449f30cdbe4a3d8c382075
Josephine Pfeiffer <hi@josie.lol>
2024-02-15 15:37:31 +0100

    bump

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-export * from '@pfeifferj/plugin-scaffolder-backend-module-kubernetes-deploy';
-export * from '@pfeifferj/plugin-scaffolder-backend-module-read-file';
 export * from './dynamic/index';
